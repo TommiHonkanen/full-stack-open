b54732aedad4cdade3220c35facbf98120b78a5d
Tommi Honkanen <tommi.honkanen01@gmail.com>
2022-09-11 19:22:23 +0300

    4.15-4.23

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,13 +1,21 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
+const User = require("../models/user")
+const jwt = require("jsonwebtoken")
 
 blogsRouter.get("/", async (_request, response) => {
-	const blogs = await Blog.find({})
+	const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 })
 	response.json(blogs)
 })
 
 blogsRouter.post("/", async (request, response) => {
 	const body = request.body
+	const token = request.token
+	const decodedToken = jwt.verify(token, process.env.SECRET)
+	if (!token || !decodedToken.id) {
+		return response.status(401).json({ error: "token missing or invalid" })
+	}
+	const user = request.user
 
 	if (body.title === undefined || body.author === undefined) {
 		return response.status(400).json({ error: "title or author missing" })
@@ -17,16 +25,39 @@ blogsRouter.post("/", async (request, response) => {
 		title: body.title,
 		author: body.author,
 		url: body.url,
-		likes: body.likes ? body.likes : 0
+		likes: body.likes ? body.likes : 0,
+		user: user._id
 	})
 
 	const savedBlog = await blog.save()
+	user.blogs = user.blogs.concat(savedBlog._id)
+	await user.save()
 	response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete("/:id", async (request, response) => {
-	await Blog.findByIdAndRemove(request.params.id)
-	response.status(204).end()
+	const token = request.token
+	const decodedToken = jwt.verify(token, process.env.SECRET)
+	if (!token || !decodedToken.id) {
+		return response.status(401).json({ error: "token missing or invalid" })
+	}
+	const user = request.user
+	const userid = user.id
+
+	const blog = await Blog.findById(request.params.id)
+
+	if (blog.user === undefined) {
+		return response.status(401).json({ error: "You are not authorized to delete this blog" })
+	}
+
+	if (blog.user.toString() === userid.toString()) {
+		await Blog.findByIdAndRemove(request.params.id)
+		response.status(204).end()
+	} else {
+		return response.status(401).json({ error: "You are not authorized to delete this blog" })
+	}
+
+	
 })
 
 blogsRouter.put("/:id", async (request, response) => {
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -52,12 +52,20 @@ const initialBlogs = [
 		url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
 		likes: 2,
 		__v: 0
-	}  
+	}
 ]
 
 beforeEach(async () => {
 	await Blog.deleteMany({})
 	await Blog.insertMany(initialBlogs)
+
+	await api
+		.post("/api/users")
+		.send({
+			"name": "test test",
+			"username": "test",
+			"password": "12345"
+		})
 })
 
 describe("retrieving blogs", () => {
@@ -90,9 +98,17 @@ describe("adding a new blog", () => {
 			url: "http://ggfdfddddddddgh.dfsg",
 			likes: 62,
 		}
+
+		const user = await api
+			.post("/api/login")
+			.send({
+				"username": "test",
+				"password": "12345"
+			})
         
 		await api
 			.post("/api/blogs")
+			.set("Authorization", "bearer " + user.body.token)
 			.send(newBlog)
 			.expect(201)
 			.expect("Content-Type", /application\/json/)
@@ -111,9 +127,17 @@ describe("adding a new blog", () => {
 			author: "Walter White",
 			url: "http://ggfdfddddddddgh.dfsg",
 		}
+
+		const user = await api
+			.post("/api/login")
+			.send({
+				"username": "test",
+				"password": "12345"
+			})
         
 		await api
 			.post("/api/blogs")
+			.set("Authorization", "bearer " + user.body.token)
 			.send(newBlog)
 			.expect(201)
 			.expect("Content-Type", /application\/json/)
@@ -129,11 +153,37 @@ describe("adding a new blog", () => {
 		const newBlog = {
 			likes: 62
 		}
+
+		const user = await api
+			.post("/api/login")
+			.send({
+				"username": "test",
+				"password": "12345"
+			})
+    
+		await api
+			.post("/api/blogs")
+			.set("Authorization", "bearer " + user.body.token)
+			.send(newBlog)
+			.expect(400)
+        
+		const blogs = await Blog.find({})
+		const blogsAtEnd = blogs.map(blog => blog.toJSON())
+    
+		expect(blogsAtEnd).toHaveLength(initialBlogs.length)
+	})
+
+	test("fails with status code 401 if token is not sent", async () => {
+		const newBlog = {
+			title: "test 2",
+			author: "Walter White",
+			url: "http://ggfdfddddddddgh.dfsg",
+		}
     
 		await api
 			.post("/api/blogs")
 			.send(newBlog)
-			.expect(400)
+			.expect(401)
         
 		const blogs = await Blog.find({})
 		const blogsAtEnd = blogs.map(blog => blog.toJSON())
@@ -146,22 +196,70 @@ describe("adding a new blog", () => {
 
 describe("deletion of a blog", () => {
 	test("succeeds with status code 204 if id is valid", async () => {
+		const user = await api
+			.post("/api/login")
+			.send({
+				"username": "test",
+				"password": "12345"
+			})
+
+		const blog = {
+			title: "for delete test",
+			author: "keys",
+			url: "ggggasdg.html",
+			likes: 62222,
+			user: "631df4e00088c2bd841cfa2e",
+		}    
+
+		await api
+			.post("/api/blogs")
+			.set("Authorization", "bearer " + user.body.token)
+			.send(blog)
+
 		const blogs = await Blog.find({})
 		const blogsAtStart = blogs.map(blog => blog.toJSON())
-		const blogToDelete = blogsAtStart[0]
-    
+		const blogToDelete = blogsAtStart.filter(blog => blog.title === "for delete test")
+
 		await api
-			.delete(`/api/blogs/${blogToDelete.id}`)
+			.delete(`/api/blogs/${blogToDelete[0].id}`)
+			.set("Authorization", "bearer " + user.body.token)
 			.expect(204)
         
 		const blogs2 = await Blog.find({})
 		const blogsAtEnd = blogs2.map(blog => blog.toJSON())
 
-		expect(blogsAtEnd).toHaveLength(initialBlogs.length - 1)
+		expect(blogsAtEnd).toHaveLength(initialBlogs.length)
 
 		const titles = blogsAtEnd.map(blog => blog.title)
 
-		expect(titles).not.toContain(blogToDelete.title)
+		expect(titles).not.toContain("for delete test")
+	})
+
+	test("fails with status code 401 if trying to delete someone else's blog", async () => {
+		const user = await api
+			.post("/api/login")
+			.send({
+				"username": "test",
+				"password": "12345"
+			})
+
+		const blogs = await Blog.find({})
+		const blogsAtStart = blogs.map(blog => blog.toJSON())
+		const blogToDelete = blogsAtStart[0]
+
+		await api
+			.delete(`/api/blogs/${blogToDelete.id}`)
+			.set("Authorization", "bearer " + user.body.token)
+			.expect(401)
+        
+		const blogs2 = await Blog.find({})
+		const blogsAtEnd = blogs2.map(blog => blog.toJSON())
+
+		expect(blogsAtEnd).toHaveLength(initialBlogs.length)
+
+		const titles = blogsAtEnd.map(blog => blog.title)
+
+		expect(titles).toContain(blogToDelete.title)
 	})
 })
 
@@ -182,8 +280,6 @@ describe("editing a blog", () => {
 		const response = await api.get("/api/blogs")
 
 		const blog = response.body.filter(blog => blog.title === "Type wars") 
-
-		console.log(blog)
 
 		expect(blog[0].likes).toBe(5)
 	})
